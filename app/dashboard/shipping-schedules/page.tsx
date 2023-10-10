f7f35843d9bbb2715a9d75203c521af4a4507d3c
sukebon <39957486+sukebon@users.noreply.github.com>
2023-10-10 23:03:49 +0900

    reset modal button

--- app/dashboard/shipping-schedules/page.tsx
+++ app/dashboard/shipping-schedules/page.tsx
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 import { Database } from '@/schema';
 import ShippingScheduleTable from './components/shipping-schedule-table';
 import { NextPage } from 'next';
-import ShippingScheduleModal from './components/shipping-schedule-modal';
+import ShippingScheduleConfirmModal from './components/shipping-schedule-confirm-modal';
 
 const ShippingSchedules: NextPage = async () => {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -26,18 +26,17 @@ const ShippingSchedules: NextPage = async () => {
     return session?.user.id;
   };
 
-
   const shippingSchedules = await getShippingSchedules();
-  const userId = await getUserId()
+  const userId = await getUserId();
 
   if (!shippingSchedules) return;
-  if(!userId) return
+  if (!userId) return;
 
   return (
     <div className="w-full max-w-[calc(1500px)] mx-auto">
       <h1 className="mt-6 text-3xl font-bold">出荷予定（発注残）</h1>
-      <ShippingScheduleModal />
-      <ShippingScheduleTable shippingSchedules={shippingSchedules} userId={userId}/>
+      <ShippingScheduleConfirmModal />
+      <ShippingScheduleTable shippingSchedules={shippingSchedules} userId={userId} />
     </div>
   );
 };
